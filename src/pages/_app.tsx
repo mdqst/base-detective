669a45b5893813bac62c36a9763e9cfc45d5689b
Dmitry <98899785+mdqst@users.noreply.github.com>
2025-11-02 19:50:09 +0300

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import "@/styles/globals.css";
+import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { WagmiConfig, http, createConfig } from "wagmi";
 import { base } from "wagmi/chains";
